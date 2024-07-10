3a8b5da1d97dcefaaa29a78f3d4818538a1c7f78
dennislavn <2946136656@qq.com>
2024-07-10 16:53:10 +0800

    激活状态显示以及渲染列表

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,28 +1,28 @@
 <script setup>
-import { getCategoryAPI } from '@/apis/category';
-import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
-import { getBannerAPI } from '@/apis/home';
+  import { getCategoryAPI } from '@/apis/category';
+  import { onMounted, ref } from 'vue';
+  import { useRoute } from 'vue-router';
+  import { getBannerAPI } from '@/apis/home';
+  import GoodsItem from '../Home/components/GoodsItem.vue';
+  // 获取数据
 
-// 获取数据
+  const categoryData = ref({})
+  const route = useRoute()
+  const getCategory = async () => {
+    const res = await getCategoryAPI(route.params.id)
+    categoryData.value = res.result
+  }
+  onMounted(() => getCategory())
 
-const categoryData = ref({})
-const route = useRoute()
-const getCategory = async()=>{
-  const res = await getCategoryAPI(route.params.id)
-  categoryData.value = res.result
-}
-onMounted(()=>getCategory())
-
-// 获取banner
-const bannerList = ref([])
-const getBanner = async()=>{
-   const res = await getBannerAPI({
-    distributionSite: '2'
-   })
-   bannerList.value = res.result
-}
-onMounted(()=> getBanner())
+  // 获取banner
+  const bannerList = ref([])
+  const getBanner = async () => {
+    const res = await getBannerAPI({
+      distributionSite: '2'
+    })
+    bannerList.value = res.result
+  }
+  onMounted(() => getBanner())
 
 </script>
 
@@ -38,103 +38,124 @@ onMounted(()=> getBanner())
       </div>
       <!-- 轮播图 -->
       <div class="home-banner">
-    <el-carousel height="500px">
-      <el-carousel-item v-for="item in bannerList" :key="item.id">
-        <img :src="item.imgUrl" alt="">
-      </el-carousel-item>
-    </el-carousel>
-  </div>
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
 
 
 <style scoped lang="scss">
-.top-category {
-  h3 {
-    font-size: 28px;
-    color: #666;
-    font-weight: normal;
-    text-align: center;
-    line-height: 100px;
-  }
+  .top-category {
+    h3 {
+      font-size: 28px;
+      color: #666;
+      font-weight: normal;
+      text-align: center;
+      line-height: 100px;
+    }
 
-  .sub-list {
-    margin-top: 20px;
-    background-color: #fff;
+    .sub-list {
+      margin-top: 20px;
+      background-color: #fff;
 
-    ul {
-      display: flex;
-      padding: 0 32px;
-      flex-wrap: wrap;
+      ul {
+        display: flex;
+        padding: 0 32px;
+        flex-wrap: wrap;
 
-      li {
-        width: 168px;
-        height: 160px;
+        li {
+          width: 168px;
+          height: 160px;
 
 
-        a {
-          text-align: center;
-          display: block;
-          font-size: 16px;
+          a {
+            text-align: center;
+            display: block;
+            font-size: 16px;
 
-          img {
-            width: 100px;
-            height: 100px;
-          }
+            img {
+              width: 100px;
+              height: 100px;
+            }
 
-          p {
-            line-height: 40px;
-          }
+            p {
+              line-height: 40px;
+            }
 
-          &:hover {
-            color: $xtxColor;
+            &:hover {
+              color: $xtxColor;
+            }
           }
         }
       }
     }
-  }
 
-  .ref-goods {
-    background-color: #fff;
-    margin-top: 20px;
-    position: relative;
+    .ref-goods {
+      background-color: #fff;
+      margin-top: 20px;
+      position: relative;
 
-    .head {
-      .xtx-more {
-        position: absolute;
-        top: 20px;
-        right: 20px;
+      .head {
+        .xtx-more {
+          position: absolute;
+          top: 20px;
+          right: 20px;
+        }
+
+        .tag {
+          text-align: center;
+          color: #999;
+          font-size: 20px;
+          position: relative;
+          top: -20px;
+        }
       }
 
-      .tag {
-        text-align: center;
-        color: #999;
-        font-size: 20px;
-        position: relative;
-        top: -20px;
+      .body {
+        display: flex;
+        justify-content: space-around;
+        padding: 0 40px 30px;
       }
     }
 
-    .body {
-      display: flex;
-      justify-content: space-around;
-      padding: 0 40px 30px;
+    .bread-container {
+      padding: 25px 0;
     }
   }
 
-  .bread-container {
-    padding: 25px 0;
-  }
-}
-.home-banner {
-  width: 1240px;
-  height: 500px;
-  margin: 0 auto;
-
-  img {
-    width: 100%;
+  .home-banner {
+    width: 1240px;
     height: 500px;
+    margin: 0 auto;
+
+    img {
+      width: 100%;
+      height: 500px;
+    }
   }
-}
 </style>
